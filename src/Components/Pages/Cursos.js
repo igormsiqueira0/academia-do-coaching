d6f4bb013ae8ee78638184ad546401c5e33b552c
Igor Marinho <igor.siqueira700@gmail.com>
2022-05-17 15:03:57 -0300

    finalizado slider de cursos na home

--- src/Components/Pages/Cursos.js
+++ src/Components/Pages/Cursos.js
@@ -2,9 +2,35 @@ import React from 'react';
 import PageBanner from '../PageBanner';
 
 const Cursos = () => {
+	const [data, setData] = React.useState([]);
+	const [categorias, setCategorias] = React.useState([]);
+
+	async function fetchData() {
+		const jsonData = await (await fetch('./cursos.json')).json();
+
+		setTimeout(() => {
+			setData(jsonData);
+		}, 500);
+	}
+
+	React.useEffect(() => {
+		fetchData();
+	}, []);
+
 	return (
 		<div>
 			<PageBanner title="Cursos" />
+
+			<nav>
+				<ul>
+					{/* {data.map(({ categoria }) => {
+						setCategorias((state) => [...state, categoria]);
+						if (categorias.includes(categoria))
+							return <li key={categoria}>{categoria}</li>;
+						return null;
+					})} */}
+				</ul>
+			</nav>
 		</div>
 	);
 };
--- src/Components/Slides/SlideCursos.js
+++ src/Components/Slides/SlideCursos.js
@@ -4,15 +4,8 @@ import Slider from 'react-slick/lib/slider';
 
 const SlideCursos = () => {
 	const [data, setData] = React.useState(null);
-
-	const settings = {
-		infinite: false,
-		speed: 500,
-		slidesToShow: 2,
-		slidesToScroll: 2,
-		prevArrow: <button className="slide-arrow slide-prev"></button>,
-		nextArrow: <button className="slide-arrow slide-next"></button>,
-	};
+	const [activeSlideBefore, setActiveSlideBefore] = React.useState(0);
+	const [activeSlideAfter, setActiveSlideAfter] = React.useState(0);
 
 	const navigateTo = useNavigate();
 
@@ -30,29 +23,71 @@ const SlideCursos = () => {
 
 	React.useEffect(() => {
 		fetchData();
-
-		const firstImage = document.querySelector('.slide-container');
-		firstImage.style.marginLeft = '0px';
 	}, []);
+
+	React.useEffect(() => {
+		setTimeout(() => {
+			const current = document.querySelector('.slick-current');
+			let dataIndex;
+
+			if (current) {
+				dataIndex = current.dataset.index;
+			}
+
+			if (current !== 0) {
+				const previous = document.querySelector(
+					`.slick-slide[data-index="${dataIndex - 1}"]`
+				);
+				const allContainers = document.querySelectorAll('.slide-container');
+				allContainers.forEach((container) => {
+					container.classList.remove('slide-previous');
+				});
+				const previousContainer = previous.querySelector('.slide-container');
+				previousContainer.classList.add('slide-previous');
+			}
+		});
+	}, [activeSlideAfter, activeSlideBefore]);
+
+	const settings = {
+		infinite: false,
+		speed: 500,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		beforeChange: (current, next) => setActiveSlideBefore(next),
+		afterChange: (current) => setActiveSlideAfter(current),
+		prevArrow: <button className="slide-arrow slide-prev"></button>,
+		nextArrow: <button className="slide-arrow slide-next"></button>,
+		responsive: [
+			{
+				breakpoint: 1199,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	};
 
 	if (data)
 		return (
-			<Slider {...settings}>
-				{data.map(({ curso, categoria, imagem }, index) => (
-					<div key={curso}>
-						<div className="slide-container" onClick={handleSlideClick}>
-							<div className="slide-img-wrapper">
-								<img src={imagem} alt={curso} />
+			<>
+				<Slider {...settings}>
+					{data.map(({ curso, categoria, imagem }) => (
+						<div key={curso}>
+							<div className="slide-container" onClick={handleSlideClick}>
+								<div className="slide-img-wrapper">
+									<img src={imagem} alt={curso} />
+								</div>
+								<div className="slide-content">
+									<span>{categoria}</span>
+									<h2>{curso}</h2>
+								</div>
+								<button onClick={handleSlideClick}></button>
 							</div>
-							<div className="slide-content">
-								<span>{categoria}</span>
-								<h2>{curso}</h2>
-							</div>
-							<button onClick={handleSlideClick}></button>
 						</div>
-					</div>
-				))}
-			</Slider>
+					))}
+				</Slider>
+			</>
 		);
 };
 
